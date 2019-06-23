94ce553dfe0281a47482753ddfef3546bcf3f3b4
abel <abel.wang@gmail.com>
2019-06-23 12:10:41 -0700

    debugging

--- IaC/provisionDnsCheckerFunction.sh
+++ IaC/provisionDnsCheckerFunction.sh
@@ -1,11 +1,21 @@
 # this creates a storage account for our back end azure function to maintain
 # state and other info for the function
 # 
-echo "create a storage account for function to maintain state and other info for the function DnsChecker"
-az storage account create --name $IAC_EXCLUSIVE_FUNCTIONDNSCHECKERSTORAGENAME --location $IAC_FUNCTIONDNSCHECKERSTORAGEREGION --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME --sku Standard_LRS
+echo "create a storage account for function to maintain state and other info for the function"
+az storage account create \
+    --name $IAC_EXCLUSIVE_FUNCTIONSTORAGEACCOUNTNAME \
+    --location $IAC_FUNCTIONSTORAGEACCOUNTREGION \
+    --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME \
+    --sku $IAC_FUNCTIONSTORAGEACCOUNTSKU
 echo ""
+
 # this creates the function app used to host the back end function
 #
-echo "create the function app for the DNS Checker "
-az functionapp create --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME --consumption-plan-location $IAC_FUNCTIONDNSCHECKERCONSUMPTIONPLANREGION --name $IAC_EXCLUSIVE_FUNCTIONDNSCHECKERNAME --storage-account $IAC_EXCLUSIVE_FUNCTIONDNSCHECKERSTORAGENAME --runtime dotnet
+echo "create the function app for the back end "
+az functionapp create \
+    --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME \
+    --consumption-plan-location $IAC_FUNCTIONCONSUMPTIONPLANREGION \
+    --name $IAC_EXCLUSIVE_FUNCTIONNAME \
+    --storage-account $IAC_EXCLUSIVE_FUNCTIONSTORAGEACCOUNTNAME \
+    --runtime $IAC_FUNCTIONRUNTIME
 echo ""
