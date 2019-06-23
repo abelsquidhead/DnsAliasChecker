58941a68c782c208b8c6931930f3de566fc4b5ff
abel <abel.wang@gmail.com>
2019-06-23 10:47:09 -0700

    moved everything to one line

--- IaC/provisionDnsCheckerFunction.sh
+++ IaC/provisionDnsCheckerFunction.sh
@@ -2,22 +2,13 @@
     # state and other info for the function
     # 
     echo "create a storage account for function to maintain state and other info for the function DnsChecker"
-    az storage account create \
-        --name $IAC_EXCLUSIVE_FUNCTIONDNSCHECKERSTORAGENAME \
-        --location $IAC_FUNCTIONDNSCHECKERSTORAGEREGION \
-        --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME \
-        --sku $IAC_FUNCTIONDNSCHECKERSTORAGESKU
+    az storage account create --name $IAC_EXCLUSIVE_FUNCTIONDNSCHECKERSTORAGENAME --location $IAC_FUNCTIONDNSCHECKERSTORAGEREGION --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME --sku $IAC_FUNCTIONDNSCHECKERSTORAGESKU
     echo ""
 
     # this creates the function app used to host the back end function
     #
     echo "create the function app for the DNS Checker "
-    az functionapp create \
-        --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME \
-        --consumption-plan-location $IAC_FUNCTIONDNSCHECKERCONSUMPTIONPLANREGION \
-        --name $IAC_EXCLUSIVE_FUNCTIONDNSCHECKERNAME \
-        --storage-account $IAC_EXCLUSIVE_FUNCTIONDNSCHECKERSTORAGENAME \
-        --runtime $IAC_FUNCTIONDNSCHECKERRUNTIME
+    az functionapp create --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME --consumption-plan-location $IAC_FUNCTIONDNSCHECKERCONSUMPTIONPLANREGION --name $IAC_EXCLUSIVE_FUNCTIONDNSCHECKERNAME --storage-account $IAC_EXCLUSIVE_FUNCTIONDNSCHECKERSTORAGENAME --runtime $IAC_FUNCTIONDNSCHECKERRUNTIME
     echo ""
 
 
