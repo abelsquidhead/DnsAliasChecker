67a31e7ded5eb22bcc44b6c2fdfa16456608c692
abel <abel.wang@gmail.com>
2019-06-23 19:00:27 -0700

    debugging

--- IaC/provisionDnsCheckerFunction.cmd
+++ IaC/provisionDnsCheckerFunction.cmd
@@ -4,10 +4,7 @@ REM
 REM This creates the storage account used by the dns checker function
 echo "create storage account to maintain state and other info for DNSChecker function"
 az storage account create --name %IAC_EXCLUSIVE_FUNCTIONDNSCHECKERSTORAGENAME% --location %IAC_FUNCTIONDNSCHECKERSTORAGEREGION% --resource-group %IAC_EXCLUSIVE_RESOURCEGROUPNAME% --sku %IAC_FUNCTIONSTORAGEACCOUNTSKU%
-echo ""
-
-REM this creates the function for the dns checker
-REM
+echo "done creating storage account"
 echo "create the function app for the back end "
 az functionapp create --resource-group %IAC_EXCLUSIVE_RESOURCEGROUPNAME% --consumption-plan-location %IAC_FUNCTIONDNSCHECKERCONSUMPTIONPLANREGION% --name %IAC_EXCLUSIVE_FUNCTIONDNSCHECKERNAME% --storage-account %IAC_EXCLUSIVE_FUNCTIONDNSCHECKERSTORAGENAME% --runtime %IAC_FUNCTIONDNSCHECKERRUNTIME%
 echo ""
