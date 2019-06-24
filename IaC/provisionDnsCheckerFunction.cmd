91188e9f8ae1ef30793562e4cba13dd767d37b94
abel <abel.wang@gmail.com>
2019-06-23 19:04:19 -0700

    debugging

--- IaC/provisionDnsCheckerFunction.cmd
+++ IaC/provisionDnsCheckerFunction.cmd
@@ -2,7 +2,7 @@ REM This script creates the infrastructure in azure for the dns checker
 REM
 
 REM This creates the storage account used by the dns checker function
-echo "create storage account to maintain state and other info for DNSChecker function"
+echo "xxxx create storage account to maintain state and other info for DNSChecker function"
 az storage account create --name %IAC_EXCLUSIVE_FUNCTIONDNSCHECKERSTORAGENAME% --location %IAC_FUNCTIONDNSCHECKERSTORAGEREGION% --resource-group %IAC_EXCLUSIVE_RESOURCEGROUPNAME% --sku %IAC_FUNCTIONSTORAGEACCOUNTSKU%
 echo "done creating storage account"
 echo "create the function app for the back end "
